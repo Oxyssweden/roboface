c9e1860ccf7905909be62fcff1b4eb259bd40174
Andreas Radloff <andreas@oxys.se>
2019-04-29 10:38:18 +0200

    meter test

--- meter.py
+++ meter.py
@@ -4,9 +4,9 @@ from aiy.pins import PIN_D
 
 led = Servo(PIN_D)
 
-    led.min()
-    sleep(1)
-    led.mid()
-    sleep(1)
-    led.max()
-    sleep(1)
+led.min()
+sleep(1)
+led.mid()
+sleep(1)
+led.max()
+sleep(1)
