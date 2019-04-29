a58340b8c9416e4eb48c933acd6452f72ea33899
Andreas Radloff <andreas@oxys.se>
2019-04-29 13:19:12 +0200

    meter test

--- meter.py
+++ meter.py
@@ -1,11 +1,12 @@
 from time import sleep
 from aiy.pins import PIN_D
 from gpiozero import LED
-from time import sleep
 
 led = LED(PIN_D)
 
 while True:
     led.on()
+    print led.value
     sleep(2)
     led.off()
+    print led.value
